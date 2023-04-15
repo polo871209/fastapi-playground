540e68f6a43be108e0afc0e889f0b4f1598a55f3
polohi <qazh0123@gmail.com>
2023-04-15 15:31:07 +0800

    CORS settings

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.router import post, user, auth, like
 
 app = FastAPI()
+
+# CORS settings
+origins = [
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['POST', 'GET', 'PUT', 'DELETE'],
+    allow_headers=["*"],
+)
+
+# add routers
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(auth.router)
