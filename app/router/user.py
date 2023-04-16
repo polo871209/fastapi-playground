c181b1d0c72d840c1b1f7217d766a0525d29f98c
polohi <qazh0123@gmail.com>
2023-04-16 19:20:26 +0800

    add user crud

--- app/router/user.py
+++ app/router/user.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Body, status, HTTPException, Path
+from fastapi import APIRouter, Body, status, HTTPException
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm.query import Query
 
 from app import schemas
 from app.database import models
 from app.database.database import GetDb
+from app.oauth2 import UserLogin
 from app.utils.hash import get_password_hash
 
 router = APIRouter(
@@ -32,9 +33,25 @@ def create_user(db: GetDb, payload: schemas.UserCreate = Body()):
     return new_user
 
 
-@router.get('/{user_id}', response_model=schemas.UserOut)
-def get_user(db: GetDb, user_id: int = Path()):
-    user = db.query(models.User).filter(models.User.id == user_id)
-    check_user_exist(user, user_id)
+@router.get('/', response_model=schemas.UserOut)
+def get_current_user(user: UserLogin, db: GetDb):
+    return db.query(models.User).filter(models.User.id == user.id).first()
 
-    return user.first()
+
+@router.put('/', response_model=schemas.UserOut, status_code=status.HTTP_202_ACCEPTED)
+def update_current_user(user: UserLogin, db: GetDb, payload: schemas.UserCreate = Body()):
+    updated_user = db.query(models.User).where(models.User.id == user.id)
+    payload.password = get_password_hash(payload.password)
+    updated_user.update(payload.dict(), synchronize_session=False)
+    db.commit()
+
+    return updated_user.first()
+
+
+# @router.delete('/', status_code=status.HTTP_204_NO_CONTENT)
+# def delete_current_user(user: UserLogin, db: GetDb) -> None:
+#     current_user = db.query(models.User).where(models.User.id == user.id)
+#     if not current_user.first():
+#         return None
+#     current_user.delete(synchronize_session=False)
+#     db.commit()
