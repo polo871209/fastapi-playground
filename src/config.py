89fcb1b956f2ac1c0a9b04886ae3c55c185e81d8
polohi <qazh0123@gmail.com>
2023-04-18 19:13:17 +0800

    update env

--- src/config.py
+++ src/config.py
@@ -2,10 +2,9 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str  # 'mysql+mysqlconnector://user:password@localhost/fastapi'
-    # JWT configs
-    SECRET_KEY: str  # '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
-    ALGORITHM: str  # 'HS256'
+    SQLALCHEMY_DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: float = 30
 
     class Config:
